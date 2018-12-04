5eb2976114578d71f8069ced027ee183053d6518
przepiorar <przepiorar@wp.pl>
2018-12-04 20:17:45 +0100

    skalowanie v2

--- Assets/Scripts/CurrentSelected2.cs
+++ Assets/Scripts/CurrentSelected2.cs
@@ -21,19 +21,25 @@ public class CurrentSelected2 : MonoBehaviour
         cardViz.gameObject.SetActive(true);
         mTransform.position = Input.mousePosition;  //zrobic sztuczke z sign
       // Debug.Log(mTransform.position.x); // pozycja myszki jest określana na podstawie rozdzielczości !!!!
-        if (mTransform.position.y <640)
+         int w = Screen.width;
+        int h = Screen.height;
+        if (mTransform.position.y <640*h/1020)
         {
-            mTransform.position += new Vector3(0, 150, 0);
+            mTransform.position += new Vector3(0, 150*h/1020, 0);
         }
         else
         {
-            mTransform.position += new Vector3(0, -150, 0);
+            mTransform.position += new Vector3(0, -150*h/1020, 0);
         }
-        if (mTransform.position.x<430)
+        if (mTransform.position.x<430*w/1920)
         {
-            mTransform.position += new Vector3(200, 0, 0);
+          mTransform.position += new Vector3(200*w/1920, 0, 0);
         }
-        Vector3 s = Vector3.one * 2;
+        Vector3 s;
+        if (w < 1000)
+           s = Vector3.one * 1.5f;
+        else
+             s = Vector3.one * 2;
         cardViz.gameObject.transform.localScale =s;
     }
 
